2e9a874077b7d93eed0c6ee8c61d9df36ec5e54c
SelB0t <botondik@gmail.com>
2023-09-23 10:09:07 +0200

    hibak kommentelese

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // N_ELEMENTS NELEMENTS helyett
+    std::cout << '1-100 ertekek duplazasa' // "" nem '' , a sor vegen hianyzik a ; es egy endl esetleg
+    for (int i = 0;) // hibas ciklus : i < N_ELEMENTS ; i++
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // igy 0-99 duplazza a szamokat
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // i < N_ELEMENTS, nem i
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // az erteket nem irja ki , hianyzik a sor vegerol a ; es egy endl esetleg
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // atlagnal jobb lenne a double tipus , 0-val kell inicializalni, hogy ne valamilyen random ertek maradjon a valtozoban
+    for (int i = 0; i < N_ELEMENTS, i++) // i++ elott ; nem sima ,
     {
-        atlag += b[i]
+        atlag += b[i] // hianyzo ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    return 0; // hianyzik a b felszabaditasa
 }
